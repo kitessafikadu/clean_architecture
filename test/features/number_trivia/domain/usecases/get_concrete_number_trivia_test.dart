3ce1550b406bc45b39fc177959c1c36dfc18ded3
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-03-26 11:04:14 +0300

    Add mock for NumberTriviaRepository in unit tests

---
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,6 @@
+import 'package:clean_architecture/features/number_trivia/data/repositories/number_trivia_repository.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
